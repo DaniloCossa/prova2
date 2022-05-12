32e541cf5816fdc9746e297b67e875a83d3f0109
CossaDanilo <Utente@Utente-PC>
2022-05-12 17:04:40 +0200

    versione 0: errata

---
+++ src/CossaDaniloClass.java
@@ -0,0 +1,48 @@
+/**
+ * CossaDaniloClass
+ * 
+ * @author <b>Cossa Danilo</b> <u>4BI</u> <i>04/05/2022</i>
+ * @version 0
+ *
+ */
+public class CossaDaniloClass {
+	/**
+	 * 
+	 * Costruttore della classe CossaDaniloClass
+	 * 
+	 */
+	public CossaDaniloClass() { 
+		
+	} 
+	
+	/**
+	 * Metodo che calcola la sommatoria da f a 0
+	 * @param f valore massimo della sommatoria
+	 * @return risultato che viene ritornato dalla funzione
+	 * 
+	 * PRIMO ERRORE: r va inizializzato a 0
+	 * 
+	 * SECONDO ERRORE: la prima condizione del ciclo for i deve essere uguale a 0
+	 * 				   la seconda condizione i<=f per andare da 0 a f
+	 * 
+	 * TERZO ERRORE: r+=i, non *
+	 */
+	
+	public int cossaMet(int f) {
+		int r = 1;
+		for(int i=f; i<0; i++) {
+			r = r * i;
+		}
+		return r;
+	}
+	
+	/**
+	 * metodo main
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		CossaDaniloClass f = new CossaDaniloClass();
+		System.out.println(f.cossaMet(5));
+	}
+}
